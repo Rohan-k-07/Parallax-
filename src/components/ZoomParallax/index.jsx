d1011af94adc00eefddf86beb7aa1545aaa5bede
Rohan-k-07 <rohan.k.codersboutique@gmail.com>
2025-03-24 10:46:24 +0530

    parallax effect

--- src/components/ZoomParallax/index.jsx
+++ src/components/ZoomParallax/index.jsx
@@ -1,78 +1,55 @@
+'use client'
 import styles from './styles.module.scss';
-import Picture1 from '../../../public/images/1.jpeg';
-import Picture2 from '../../../public/images/2.jpeg';
-import Picture3 from '../../../public/images/3.jpg';
-import Picture4 from '../../../public/images/4.jpg'
-import Picture5 from '../../../public/images/5.jpg'
-import Picture6 from '../../../public/images/6.jpg'
-import Picture7 from '../../../public/images/7.jpeg'
+import Picture2 from '../../../public/images/women.jpg';
+import Picture3 from '../../../public/images/car.jpg';
+import Picture4 from '../../../public/images/dog.jpg';
 import Image from 'next/image';
-import { useScroll, useTransform, motion} from 'framer-motion';
+import { useScroll, useTransform, motion } from 'framer-motion';
 import { useRef } from 'react';
 
 export default function Index() {
-    
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
         target: container,
         offset: ['start start', 'end end']
-    })
+    });
 
-    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
-    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
-    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
-    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
-    const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+    // Scaling for parallax effect
+    const scaleVideo = useTransform(scrollYProgress, [0, 1], [1, 1.3]);
+    const scaleImage1 = useTransform(scrollYProgress, [0, 1], [1, 1.8]);
+    const scaleImage2 = useTransform(scrollYProgress, [0, 1], [1, 2]);
+    const scaleImage3 = useTransform(scrollYProgress, [0, 1], [1, 1.6]);
+    const clipPath = useTransform(scrollYProgress, [0, 1], ["inset(40%)", "inset(0%)"]);
 
     const pictures = [
-        {
-            src: Picture1,
-            scale: scale4
-        },
-        {
-            src: Picture2,
-            scale: scale5
-        },
-        {
-            src: Picture3,
-            scale: scale6
-        },
-        {
-            src: Picture4,
-            scale: scale5
-        },
-        {
-            src: Picture5,
-            scale: scale6
-        },
-        {
-            src: Picture6,
-            scale: scale8
-        },
-        {
-            src: Picture7,
-            scale: scale9
-        }
-    ]
+        { src: Picture2, scale: scaleImage1 },
+        { src: Picture3, scale: scaleImage2 },
+        { src: Picture4, scale: scaleImage3 }
+    ];
 
     return (
         <div ref={container} className={styles.container}>
             <div className={styles.sticky}>
                 {
-                    pictures.map( ({src, scale}, index) => {
-                        return <motion.div key={index} style={{scale}} className={styles.el}>
-                            <div className={styles.imageContainer}>
-                                <Image
-                                    src={src}
-                                    fill
-                                    alt="image"
-                                    placeholder='blur'
-                                />
-                            </div>
+                    pictures.map(({ src, scale }, index) => (
+                        <motion.div key={index} style={{ scale }} className={styles.el}>
+                            <motion.div className={styles.imageContainer}>
+                                <Image src={src} fill alt="image" />
+                            </motion.div>
                         </motion.div>
-                    })
+                    ))
                 }
+
+                <motion.div className={styles.VideoContainer} style={{ scale: scaleVideo }}>
+                    <motion.video
+                        src="/images/video2.mp4"
+                        style={{ clipPath }}
+                        autoPlay
+                        muted
+                        loop
+                    />
+                </motion.div>
             </div>
         </div>
-    )
+    );
 }
